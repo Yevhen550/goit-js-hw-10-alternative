6182e74ce437cc957d884483b696f25d2a12b450
Yevhen Liaskivskyi <benatanlev@icloud.com>
2024-05-14 18:10:55 +0300

    test drive

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('l');
+let globalVariable; // undefined
+
+// Initializing data fetching
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(users => {
+    console.log('users inside then callback: ', users);
+
+    // Writing the result to a global variable
+    globalVariable = users;
+
+    // Everything is ok here, the data is in the variable
+    console.log('globalVariable inside fetch callback: ', globalVariable);
+  });
+
+// No async data here
+console.log('globalVariable outside fetch: ', globalVariable); // undefined
