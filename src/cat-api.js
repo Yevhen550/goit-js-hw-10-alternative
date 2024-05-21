8a92da07d425a4462358c4d3df342b4c758b96d9
Yevhen Liaskivskyi <benatanlev@icloud.com>
2024-05-21 19:40:25 +0300

    slim select use

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,26 +1,14 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const URL = 'https://api.thecatapi.com/v1';
-const KEY =
+const BASE_URL = 'https://api.thecatapi.com/v1';
+const API_KEY =
   'live_rHTDikZpK6TeyKG7KsPPULvszGbkefM2L66OWPKA8kQlktY6Q7DLxTyQr2WD6IP3';
-axios.defaults.headers.common['x-api-key'] = KEY;
+// axios.defaults.headers.common['x-api-key'] = API_KEY;
 
-const url = `${URL}/breeds?api_key=${KEY}`;
-
-let storedBreeds = [];
+const url = `${BASE_URL}/breeds?api_key=${API_KEY}`;
 
 export function fetchBreeds() {
-  console.log('Порода');
-  return axios
-    .get(url)
-    .then(resolve => {
-      const arrData = resolve.data;
-      console.log(arrData);
-      return arrData.map(({ id }) => {
-        storedBreeds.push({ value: id });
-      });
-    })
-    .catch(error => console.log(error));
+  return fetch(url).then(resolve => resolve.json());
 }
 
 export function fetchCatByBreed(breedId) {
--- src/index.js
+++ src/index.js
@@ -1,61 +1,30 @@
-// import axios from 'axios';
-// // import SlimSelect from 'slim-select';
-
-// axios.defaults.headers.common['x-api-key'] =
-//   'live_rHTDikZpK6TeyKG7KsPPULvszGbkefM2L66OWPKA8kQlktY6Q7DLxTyQr2WD6IP3';
-
-// const block = document.querySelector('.cat-info');
-// const selectBreed = document.querySelector('.breed-select');
-
-// // new SlimSelect({
-// //   select: selectBreed,
-// // });
-
-// function createCat(data) {
-//   return data
-//     .map(({ breeds, id, url, width, height }) => {
-//       const breedName = breeds.length > 0 ? breeds[0].name : 'Unknown breed';
-//       const text =
-//         breeds.length > 0 ? breeds[0].description : 'No description available';
-
-//       return `<div class="cat-card">
-//                 <img src="${url}" alt="${id}" width="${width}" height="${height}" class="cat-image" />
-//                 <div class="cat-details">
-//                   <h2 class="cat-name">${breedName}</h2>
-//                   <p class="cat-description">${text}</p>
-//                 </div>
-//               </div>`;
-//     })
-//     .join('');
-// }
-
-// async function fetchCats() {
-//   try {
-//     const response = await axios.get(
-//       'https://api.thecatapi.com/v1/images/search?limit=1&breed_ids=beng'
-//     );
-//     const data = response.data;
-//     const catHtml = createCat(data);
-//     block.insertAdjacentHTML('beforeend', catHtml);
-//   } catch (error) {
-//     console.error('Error fetching cat data:', error);
-//   }
-// }
-
-// fetchCats();
-
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
 
 const selectBreed = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
 const block = document.querySelector('.cat-info');
 
-new SlimSelect({
-  select: selectBreed,
-});
+let arrBreedId = [];
 
-console.log(fetchBreeds());
-fetchCatByBreed();
+selectBreed.addEventListener('change', onSelectValue);
+
+function onSelectValue(ev) {
+  ev.preventDefault();
+}
+
+fetchBreeds().then(data => {
+  data.map(el => {
+    arrBreedId.push({ text: el.name, value: el.id });
+  });
+  new SlimSelect({
+    select: selectBreed,
+    data: arrBreedId,
+    settings: {
+      searchPlaceholder: 'Шукаю котика',
+    },
+  });
+});
