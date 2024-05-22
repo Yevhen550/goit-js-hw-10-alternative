cadfabe3d278ace0f8b1151bf0a4619d42bb321a
Yevhen Liaskivskyi <benatanlev@icloud.com>
2024-05-22 20:05:26 +0300

    ssrfgs

--- src/index.js
+++ src/index.js
@@ -30,8 +30,26 @@ function onSelectValue(ev) {
   // ev.preventDefault();
 
   const breedId = ev.currentTarget.value;
-  fetchCatByBreed(breedId).then(data => {
-    const img = data[0].url;
-    console.log(img);
-  });
+  fetchCatByBreed(breedId)
+    .then(data => {
+      const { url, breeds } = data[0];
+      const { name, temperament, description, id } = breeds[0];
+      return `
+<div class="cat-card">
+  <img
+    src="${url}"
+    alt="${id}"
+    width="${width}"
+    height="${height}"
+    class="cat-image"
+  />
+
+  <div class="cat-details">
+    <h2 class="cat-name">${name}</h2>
+    <p class="cat-description">${description}</p>
+  </div>
+</div>
+`;
+    })
+    .join('');
 }
