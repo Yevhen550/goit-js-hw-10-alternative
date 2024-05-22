1609d45b3e0188081467990777d791265c2c662a
Yevhen Liaskivskyi <benatanlev@icloud.com>
2024-05-22 11:01:46 +0300

    su

--- src/cat-api.js
+++ src/cat-api.js
@@ -5,12 +5,14 @@ const API_KEY =
   'live_rHTDikZpK6TeyKG7KsPPULvszGbkefM2L66OWPKA8kQlktY6Q7DLxTyQr2WD6IP3';
 // axios.defaults.headers.common['x-api-key'] = API_KEY;
 
-const url = `${BASE_URL}/breeds?api_key=${API_KEY}`;
-
 export function fetchBreeds() {
-  return fetch(url).then(resolve => resolve.json());
+  return fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`).then(resolve =>
+    resolve.json()
+  );
 }
 
 export function fetchCatByBreed(breedId) {
-  console.log('По ID');
+  return fetch(
+    `${BASE_URL}/images/search?breed_ids=${breedId}&api_key=${API_KEY}`
+  ).then(response => response.json());
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import { fetchBreeds, fetchCatByBreed } from './cat-api';
-
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 const selectBreed = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
@@ -10,21 +9,26 @@ const block = document.querySelector('.cat-info');
 
 let arrBreedId = [];
 
+fetchBreeds()
+  .then(data => {
+    data.map(el => {
+      arrBreedId.push({ text: el.name, value: el.id });
+    });
+    new SlimSelect({
+      select: selectBreed,
+      data: arrBreedId,
+      settings: {
+        searchPlaceholder: 'Шукаю котика',
+      },
+    });
+  })
+  .catch(error => console.log(error));
+
 selectBreed.addEventListener('change', onSelectValue);
 
 function onSelectValue(ev) {
-  ev.preventDefault();
-}
+  // ev.preventDefault();
 
-fetchBreeds().then(data => {
-  data.map(el => {
-    arrBreedId.push({ text: el.name, value: el.id });
-  });
-  new SlimSelect({
-    select: selectBreed,
-    data: arrBreedId,
-    settings: {
-      searchPlaceholder: 'Шукаю котика',
-    },
-  });
-});
+  const breedId = ev.currentTarget.value;
+  fetchCatByBreed(breedId).then(data => console.log(data));
+}
